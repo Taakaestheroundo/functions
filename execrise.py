df1abde7e47f97222fd77ba44bfe9bd525aa0472
Taakaestheroundo <144691445+Taakaestheroundo@users.noreply.github.com>
2023-10-11 07:29:49 -0700

    Add files via upload

---
+++ execrise.py
@@ -0,0 +1,23 @@
+# write apython program to reverse astring{1,2,3,4,a,b,c,d}
+def mystring():
+    str = "1,2,3,4,a,b,c,d"
+    reseveredstring = (str)
+    for f in reversed(str):
+        print(f)
+mystring("1,2,3,4,a,b,c,d")  
+
+
+
+# write apython function to multipy all the numbers in a list
+# list = {8,2,3,-1,7}
+def product_of_all_list_numbers(a,b,c,d,e):
+    output = (a*b*c*d*e)
+    print(output)
+product_of_all_list_numbers(8,2,3,-1,7)  
+
+
+# write apython program to print even numbers from the given list{1,2,3,4,5,6,7,8,9}
+def even_numbers_in_list(a,b,c,d,e,f,g,h,k):
+    even_list=[b,d,f,h]
+    print(even_list)
+even_numbers_in_list(1,2,3,4,5,6,7,8,9)    
